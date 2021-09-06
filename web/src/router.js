4c88c5ae9b86e7ec19f1ae3d9aeeb4b73ad06c9b
钟艺 <2303444758@qq.com>
2021-09-06 23:38:23 +0800

    refactor:添加路由懒加载

--- web/src/router.js
+++ web/src/router.js
@@ -1,16 +1,17 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home/Home.vue";
-import SensorList from "./views/Home/SensorList.vue";
-import AddSensor from "./views/Home/AddSensor.vue";
-import SimSensor from "./views/Home/SimSensor.vue";
-import WarnLogs from "./views/Home/WarnLogs.vue";
-import ControlLogs from "./views/Home/ControlLogs.vue";
-import ControlSensor from "./views/Home/ControlSensor.vue";
-import Login from "./views/Login/Login.vue";
-import UserManagement from "./views/Home/UserManagement.vue";
-import Registered from "./views/Login/Registered.vue";
-import VisualSensor from "./views/Home/VisualSensor.vue";
+const Home = () => import("./views/Home/Home.vue");
+const SensorList = () => import("./views/Home/SensorList.vue");
+const AddSensor = () => import("./views/Home/AddSensor.vue");
+const SimSensor = () => import("./views/Home/SimSensor.vue");
+const WarnLogs = () => import("./views/Home/WarnLogs.vue");
+const ControlLogs = () => import("./views/Home/ControlLogs.vue");
+const ControlSensor = () => import("./views/Home/ControlSensor.vue");
+const UserManagement = () => import("./views/Home/UserManagement.vue");
+const VisualSensor = () => import("./views/Home/VisualSensor.vue");
+
+const Login = () => import("./views/Login/Login.vue");
+const Registered = () => import("./views/Login/Registered.vue");
 
 Vue.use(Router);
 
